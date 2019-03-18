d46bbe99fdc4a4767b5fdbe5f4ea38f8dba39ad5
Tim Fischer <emailtimfischer@gmail.com>
2019-03-17 20:49:01 -0500

    Adjust linting rules

---
+++ src/views/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="home">homee</h1>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      test: "test data!"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  color: orange;
+}
+</style>
+
+
