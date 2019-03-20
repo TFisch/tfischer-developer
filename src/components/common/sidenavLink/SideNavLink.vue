e4225400edb70a4b553f192aa0b1d12dbbee8a15
Tim Fischer <emailtimfischer@gmail.com>
2019-03-19 23:44:47 -0500

    Create reusable link component

---
+++ src/components/common/sidenavLink/SideNavLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link class="side-nav-link" :to="url">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "SideNavLink",
+  props: ["url"]
+};
+</script>
+<style scoped>
+.side-nav-link {
+  text-decoration: none;
+}
+</style>
+
